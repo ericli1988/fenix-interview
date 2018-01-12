c30329d94af53ae5ae87b7ad13035b757cf1c4c9
eric li <eric@example.com>
2018-01-12 12:22:06 +0300

    Post request

--- core.js
+++ core.js
@@ -1,7 +1,34 @@
 var app = angular.module('basicApp', []);
-app.controller('homeController', function ($scope) {
+app.controller('homeController', function ($scope, $http) {
    $scope.AccountNumber = "";
    $scope.PaymentReference = "";
    $scope.PaymentDate = "";
    $scope.PaymentAmount = "";
+   $scope.status = "";
+   
+   
+   $scope.submit = function() {
+	 $scope.status = "";
+	 var data = $.param({
+                accountNumber: $scope.accountNumber,
+                paymentReference: $scope.paymentReference,
+				paymentDate: $scope.paymentDate,
+				paymentAmount: $scope.paymentAmount
+
+     });
+
+	 $http.post('http://localhost:3001', data, config)
+	   .then(
+		   function(response){
+			 // success callback
+			 $scope.status = "Successfully paid";
+		   }, 
+		   function(response){
+			 // failure callback
+			 $scope.status = "fail to pay.";
+		   }
+		);  
+   }
+   
+   
 });
